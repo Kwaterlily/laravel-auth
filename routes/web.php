7b74ae291c4edc3edcc97f8bbf86aafe743a6e40
liviaAugusta <liviamattoni@gmail.com>
2022-10-14 00:24:28 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('guest.home');
+});
+
+Auth::routes();
+
+// Rotte protette dell'amministrazione
+Route::middleware('auth')
+->prefix('admin')
+->namespace('Admin')
+->name('admin')
+->group(function(){
+    // home amministratore
+    Route::get('/', 'HomeController@index')->name('home');
+
+// Resource POST
+// Route::resource('posts', 'PostController');
+});
+
+
+
